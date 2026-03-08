752b21afed774f590e076aeee4fefb2abfce0ed2
Yash Sinha <131554276+YashSinha23@users.noreply.github.com>
2026-03-08 18:46:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
@@ -2,36 +2,29 @@ class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
 
         PriorityQueue<int[]> heap = new PriorityQueue<>(
-                (a, b) -> {
-                    int s1 = nums1[a[0]] + nums2[a[1]];
-                    int s2 = nums1[b[0]] + nums2[b[1]];
-                    return s2 - s1;
-                });
+            (a,b) -> (nums1[a[0]] + nums2[a[1]]) -
+                     (nums1[b[0]] + nums2[b[1]])
+        );
 
-        for (int i = 0; i < nums1.length; i++) {
-            for (int j = 0; j < nums2.length; j++) {
-                int sum = nums1[i] + nums2[j];
-                if(heap.size() < k){
-                    heap.add(new int[] { i, j });
-                }
-                else{
-                    int[] top = heap.peek();
-                    int topSum = nums1[top[0]] + nums2[top[1]];
-
-                    if(sum < topSum){
-                        heap.poll();
-                        heap.add(new int[] { i, j});
-                    }else{
-                        break;
-                    }
-                }
-
-            }
+        for(int i = 0; i < nums1.length && i < k; i++){
+            heap.add(new int[]{i,0});
         }
+
         List<List<Integer>> result = new ArrayList<>();
-        while(!heap.isEmpty()){
+
+        while(k > 0 && !heap.isEmpty()){
+
             int[] pair = heap.poll();
-            result.add(Arrays.asList(nums1[pair[0]], nums2[pair[1]]));
+            int i = pair[0];
+            int j = pair[1];
+
+            result.add(Arrays.asList(nums1[i], nums2[j]));
+
+            if(j + 1 < nums2.length){
+                heap.add(new int[]{i , j+1});
+            }
+
+            k--;
         }
 
         return result;
