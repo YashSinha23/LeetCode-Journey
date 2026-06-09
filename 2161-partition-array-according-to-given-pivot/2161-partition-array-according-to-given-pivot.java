29fa6253e346c4e333cc3bf92d40c8ede6d8b52d
Yash Sinha <131554276+YashSinha23@users.noreply.github.com>
2026-06-10 00:44:53 +0530

    Time: 5 ms (73.5%), Space: 160.2 MB (21.15%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -1,35 +1,23 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        List<Integer> less = new ArrayList<>();
-        List<Integer> more = new ArrayList<>();
-        List<Integer> equal = new ArrayList<>();
+        int[] ans = new int[nums.length];
 
-        for(int i=0; i<nums.length; i++){
-            if(nums[i] == pivot){
-                equal.add(nums[i]);
-            }else if(nums[i] > pivot){
-                more.add(nums[i]);
-            }else{
-                less.add(nums[i]);
+        int left = 0;
+        for (int num : nums) {
+            if (num < pivot) {
+                ans[left++] = num;
             }
         }
-        int size = less.size();
-        int j = 0;
-        for(int i=0; i<less.size(); i++){
-            nums[i] = less.get(j++);
+        for (int num : nums) {
+            if (num == pivot) {
+                ans[left++] = num;
+            }
         }
-
-        size = equal.size() + less.size();
-        j = 0;
-        for(int i=less.size(); i<size; i++){
-            nums[i] = equal.get(j++);
+        for (int num : nums) {
+            if (num > pivot) {
+                ans[left++] = num;
+            }
         }
-
-        size = equal.size() + less.size() + more.size();
-        j = 0;
-        for(int i=equal.size() + less.size(); i<size; i++){
-            nums[i] = more.get(j++);
-        }
-        return nums;
+        return ans;
     }
 }
