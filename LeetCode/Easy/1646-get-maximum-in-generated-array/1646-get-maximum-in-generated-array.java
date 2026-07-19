d1d4b7ab601a2abd3f1f611a2ff7a91f820cea44
Yash Sinha <131554276+YashSinha23@users.noreply.github.com>
2026-07-20 00:50:36 +0530

    07-20-2026 1646-get-maximum-in-generated-array Easy

---
+++ LeetCode/Easy/1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int getMaximumGenerated(int n) {
+        int[] nums = new int[n+1];
+        if(n==0){
+            return 0;
+        }
+        nums[0] = 0;
+        nums[1] = 1;
+        for(int i=1; 2*i <= n; i++){
+            nums[2*i] = nums[i];
+            if(2*i+1 <= n){
+                nums[2*i+1] = nums[i] + nums[i+1];
+            }
+        }
+
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] > max){
+                max = nums[i];
+            }
+        }
+
+        return max;
+    }
+}
