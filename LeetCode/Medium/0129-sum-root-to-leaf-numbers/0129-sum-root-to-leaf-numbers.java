0fdc4f2110f6dabccbf563473d2195b4786727f8
Yash Sinha <131554276+YashSinha23@users.noreply.github.com>
2026-07-25 20:57:11 +0530

    07-25-2026 0129-sum-root-to-leaf-numbers Medium

---
+++ LeetCode/Medium/0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int sum = 0;
+    public int sumNumbers(TreeNode root) {
+        helper(root,"");
+        return sum;
+    }
+
+    public TreeNode helper(TreeNode root, String str){
+        if(root == null){
+            return null;
+        }
+
+        int val = root.val;
+        str += val;
+
+        if(root.left == null && root.right==null){
+            int temp = Integer.parseInt(str);
+            sum += temp;
+        }
+        helper(root.left, str);
+        helper(root.right, str);
+
+        return null;
+    }
+}
